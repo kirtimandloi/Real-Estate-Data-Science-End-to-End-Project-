86389b5eefbad522ce298126ae6979205dabf59d
Kirti Mandloi <159609273+kirtimandloi@users.noreply.github.com>
2025-06-19 21:37:42 +0530

    Update main.py

--- Real Estate APP/main.py
+++ Real Estate APP/main.py
@@ -5,6 +5,7 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Welcome to HomeSense AI! 👋")
+st.write("## - Because finding the right home should feel smart and right.")
 
 st.sidebar.success("Select a demo above.")
